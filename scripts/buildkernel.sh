ddeac8aefcd34588879b49b7562da56dbe2bee21
Markus Dobel <markus.dobel@senacor.com>
2018-12-18 10:39:11 +0100

    Make kernel build verbose, so travis does not kill it due to no output

--- scripts/buildkernel.sh
+++ scripts/buildkernel.sh
@@ -10,5 +10,4 @@ cp $HOME/rpmbuild/PATCHES/*.diff $HOME/rpmbuild/SOURCES/
 
 pushd $HOME/rpmbuild/SPECS \
   && patch -p1 < ../PATCHES/kernel.spec.patch \
-  && time rpmbuild -bb --without debuginfo --without debug --target=$(uname -m) kernel.spec ; popd
-
+  && time rpmbuild -bb --with verbose --without debuginfo --without debug --target=$(uname -m) kernel.spec ; popd
